e1f4c8921183b0e6c20669f0baa091bbcf7bbf5a
LucasT01 <106709198+LucasT01@users.noreply.github.com>
2025-04-15 00:04:21 -0300

    Update NumberingSimulator.cpp
    Implementação de nova função, estruturação do cabeçalho de funções para serem implementadas, correções e comentários.
    Execução sem erros.

--- NumberingSimulator.cpp
+++ NumberingSimulator.cpp
@@ -1,21 +1,40 @@
 #include <iostream>
 #include <string>
+#include <cmath>
 
 using namespace std;
 
 //Cabeçalho das funções.
 string DecimalToBinary(int numero);
+int BinaryToDecimal(string binario);
 
+// int OctalToDecimal(string octal);
+// string DecimalToOctal(int decimal);
+// int HexadecimalToDecimal(string hexa);
+// string DecimalToHexadecimal(int decimal);
+
+// string BinaryToOctal(string binary);
+// string BinaryToHexadecimal(string binary);
 
 int main (){
 
-  //Recebe o número decimal para binário, converte e mostra na tela.
+  //Menu a ser implementado, contendo: opções de conversão e operações.
+  
+  //Recebe o número decimal, converte para binario e mostra na tela.
     cout << "Digite um número decimal: ";
     int decimal;
     cin >> decimal;
 
     string binario = DecimalToBinary(decimal);
     cout << "Binario: " << binario << endl;
+	
+	
+  //Recebe o número binario, converte para decimal e mostra na tela.
+	  cout << "Digite um numero binario: ";
+      cin >> binario;
+
+    decimal = BinaryToDecimal(binario);
+    cout << "Decimal de " << binario << ": " << decimal << endl;
   
     return 0;
 }
@@ -37,4 +56,17 @@ binario = bit + binario; // Coloca o bit encontrado à esquerda no numero binari
 numero /= 2; // Divide o número por 2 e armazena nele mesmo.
 }
 return binario; 
+}
+
+//Função para converter um numero binario em decimal.
+int BinaryToDecimal(string binario) {
+    int decimal = 0;
+	
+    //
+    for (int i = 0; i < binario.size(); i++) {
+        if (binario[i] == '1') {
+            decimal += pow(2, binario.size() - 1 - i);
+        }
+    }
+	return decimal;
 }
